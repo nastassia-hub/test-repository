89aa4923804af9048f4f0b07f9fb0664ad772e9b
nastassia-hub <56607001+nastassia-hub@users.noreply.github.com>
2019-11-22 09:08:39 +0300

    Add files via upload

--- zones_check.py
+++ zones_check.py
@@ -5,45 +5,42 @@ import time
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
-    request.addfinalizer(wd.quit)
-    return wd
+	wd = webdriver.Chrome()
+	request.addfinalizer(wd.quit)
+	return wd
 
 
 def test_example(driver):
-    driver.get("http://localhost/litecart/admin")
-    driver.maximize_window()
+	driver.get("http://localhost/litecart/admin")
+	driver.maximize_window()
 
-    # log in
-    login = driver.find_element_by_xpath('//*[@id="box-login"]//tr[1]/td[2]/span/input')
-    login.send_keys("admin")
-    password = driver.find_element_by_xpath('//*[@id="box-login"]//tr[2]/td[2]/span/input')
-    password.send_keys("admin")
-    button = driver.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button')
-    button.click()
+	# log in
+	login = driver.find_element_by_xpath('//*[@id="box-login"]//tr[1]/td[2]/span/input')
+	login.send_keys("admin")
+	password = driver.find_element_by_xpath('//*[@id="box-login"]//tr[2]/td[2]/span/input')
+	password.send_keys("admin")
+	button = driver.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button')
+	button.click()
 
-    # test
-    zones = driver.find_element_by_xpath("//*[@id='box-apps-menu']/li[6]/a")
-    zones.click()
-    time.sleep(3)
-    item = driver.find_elements_by_xpath("//*[@class='row']//td[3]/a")
-    for i in range(0, len(item)):
-        item[i].click()
-        time.sleep(2)
-        zones_ = driver.find_elements_by_xpath("//*[@id='table-zones']//tr/td[3]/select")
-        s = []
-        for zone in zones_:
-            zone.is_selected()
-            if True:
-                a = zone.get_attribute("textContent")
-                s.append(a)
-        print(s)
-        new_s = sorted(s)
-        if new_s == s:
-            print("Countries are in alphabet order")
-        else:
-            print("Countries aren't in alphabet order")
-        zones_ = driver.find_elements_by_xpath("//*[@id='table-zones']//tr/td[3]/select")
-        driver.back()
-        time.sleep(2)
-        item = driver.find_elements_by_xpath("//*[@class='row']//td[3]/a")
+	# test
+	zones = driver.find_element_by_xpath("//*[@id='box-apps-menu']/li[6]/a")
+	zones.click()
+	time.sleep(3)
+	item = driver.find_elements_by_xpath("//*[@class='row']//td[3]/a")
+	for i in range(0, len(item)):
+		item[i].click()
+		time.sleep(2)
+		zones_ = driver.find_elements_by_css_selector("select[name*=zone_code]>option[selected]")
+		s = []
+		for j in range(0, len(zones_) - 1):
+			a = zones_[j].get_attribute("textContent")
+			s.append(a)
+		new_s = sorted(s)
+		if new_s == s:
+			print("Countries are in alphabet order")
+		else:
+			print("Countries aren't in alphabet order")
+		zones_ = driver.find_elements_by_xpath("//*[@id='table-zones']//tr/td[3]/select")
+		driver.back()
+		time.sleep(2)
+		item = driver.find_elements_by_xpath("//*[@class='row']//td[3]/a")
